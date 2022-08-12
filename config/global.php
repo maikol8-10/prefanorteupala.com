a3b2f91bfeca68a7b3036b2b02abc249078673f7
Michael Vallejos <maikol8-10@hotmail.com>
2022-08-12 16:17:42 -0600

    Minor Change

--- config/global.php
+++ config/global.php
@@ -3,16 +3,18 @@
 define("DB_HOST", "localhost");
 
 //Nombre de la base de datos
-define("DB_NAME", "prefanorteDB");
+//define("DB_NAME", "prefanorteDB");
+define("DB_NAME", "u888592833_prefanorte");
 
 //Usuario de la base de datos
-define("DB_USERNAME", "root");
+//define("DB_USERNAME", "root");
+define("DB_USERNAME", "u888592833_prefanorte");
 
 //Contraseña del usuario de base de datos
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "O2~fELJ2lw|");
 
 //Definicion de codificación de los caracteres
 define("DB_ENCODE", "utf8");
 
 //Definicion de nombre del proyecto
-define("PRO_NOMBRE", "App Steph Boutique");
+define("PRO_NOMBRE", "Prefa Norte Upala");
